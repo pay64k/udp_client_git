2b545b595c34feabf43e615cf979d87b9c04a178
pay64k <pay64k@gmail.com>
2014-12-22 16:47:18 +0100

    v01

---
+++ src/ws/dtu/Packet.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.dtu;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author Kubala
+ */
+public class Packet {
+    
+}
---
+++ src/ws/dtu/QuoteClient.java
@@ -0,0 +1,54 @@
+package ws.dtu;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class QuoteClient {
+    public static void main(String[] args) throws IOException {
+
+
+        DatagramSocket socket = new DatagramSocket();
+        //setup timer timeout in milliseconds:
+        Timer timer = new Timer(5000);
+        //start timer:
+        //timer.start();
+        
+        
+        // send request
+        String test="TEST11";
+        byte[] buf_transmit = new byte[256];
+        byte[] buf_recieve = new byte[256];
+        buf_transmit=test.getBytes();
+       
+        
+        InetAddress address = InetAddress.getByName("127.0.0.1");
+        DatagramPacket packet = new DatagramPacket(buf_transmit, buf_transmit.length, address, 4445);
+        for(int i=0; i<1;i++)
+        {
+        socket.send(packet);
+        System.out.println("Sending: " + new String(packet.getData(), 0, packet.getLength() ));
+        
+            // get response
+        packet = new DatagramPacket(buf_recieve, buf_recieve.length);
+        socket.receive(packet);
+        System.out.println("Current timeout: " + timer.getTimer());
+        timer.reset();
+        
+        System.out.println("Current timeout: " + timer.getTimer());
+        
+            
+
+	    // display response
+        String received = new String(packet.getData(), 0, packet.getLength());
+        System.out.println("Recieved: " + received);
+        }
+        
+        socket.close();
+        int time=0;
+        while (true) {            
+            time++;
+            System.out.println(time/1000);
+        }
+    }
+}
