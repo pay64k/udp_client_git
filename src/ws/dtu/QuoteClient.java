97ceecd58254ea96d8514e3136b42c58fce74c10
pay64k <pay64k@gmail.com>
2014-12-22 23:36:47 +0100

    v06

--- src/ws/dtu/QuoteClient.java
+++ src/ws/dtu/QuoteClient.java
@@ -11,17 +11,23 @@ public class QuoteClient {
         static State currentState;
         static State nextState=State.START;
     //---------------------------------
-        
+             static Timer timer = new Timer(5000);   
     public static void main(String[] args) throws IOException {
         
-        Timer timer = new Timer(5000);
+
         //start timer:
         timer.start();
         DatagramSocket socket = new DatagramSocket();
         byte[] buf = new byte[256];
         String data=null;
-        InetAddress address = InetAddress.getByName("127.0.0.1");
-        DatagramPacket packet;
+        InetAddress address = InetAddress.getByName("127.0.0.1");////enter ip here
+        DatagramPacket send_packet;
+        DatagramPacket recv_packet;
+        int pkt_amount=0;
+        ArrayList recv_seqList=new ArrayList();
+        ArrayList recv_dataList=new ArrayList();
+        Integer recvd_counter=0;
+        int pkt_timeout = 300;
         
     while(true){
         currentState=nextState;
@@ -31,30 +37,40 @@ public class QuoteClient {
             case START:
                 data="REQUEST:";
                 buf=data.getBytes();
-                packet = new DatagramPacket(buf, buf.length, address, 4445);
-                System.out.println("Sending: " + new String(packet.getData(), 0, packet.getLength() ));
-                socket.send(packet);
-                timer.reset();
+                send_packet = new DatagramPacket(buf, buf.length, address, 4445);
+                System.out.println("Sending: " + new String(send_packet.getData(), 0, send_packet.getLength() ));
+                socket.send(send_packet);
+                //timer.reset();
                 nextState=State.WFR1;
                 buf = new byte[256];
                 break;
                 
             case WFR1:
-                packet = new DatagramPacket(buf, buf.length);
-                socket.setSoTimeout(200);//-----------
-                socket.receive(packet);
-                String received = new String(packet.getData(), 0, packet.getLength());
+                buf = new byte[256];
+                recv_packet = new DatagramPacket(buf, buf.length);
+                socket.setSoTimeout(pkt_timeout);//----------- timeout for a packet which is not arriving
+                try {
+                    socket.receive(recv_packet);
+                } catch (Exception e) {
+                    data="isNAK:";
+                    buf=data.getBytes();
+                    send_packet = new DatagramPacket(buf, buf.length, address, 4445);
+                    System.out.println("Sending: " + new String(send_packet.getData(), 0, send_packet.getLength() ));
+                    socket.send(send_packet);
+                }
+                
+                String received = new String(recv_packet.getData(), 0, recv_packet.getLength());
                 System.out.println("Recieved: " + received);
                 
                 if (received.startsWith("pkt_amount:")) {
-                    int pkt_amount=Integer.parseInt(received.substring(11));
+                    pkt_amount=Integer.parseInt(received.substring(11));
                     System.out.println("Amount of packets(int): " + pkt_amount);
                     
                     data="ACK";
                     buf=data.getBytes();
-                    packet = new DatagramPacket(buf, buf.length, address, 4445);
-                    System.out.println("Sending: " + new String(packet.getData(), 0, packet.getLength() ));
-                    socket.send(packet);
+                    send_packet = new DatagramPacket(buf, buf.length, address, 4445);
+                    System.out.println("Sending: " + new String(send_packet.getData(), 0, send_packet.getLength() ));
+                    socket.send(send_packet);
                     
                     nextState=State.REC_STREAM; 
                     timer.reset();
@@ -64,6 +80,44 @@ public class QuoteClient {
                 break;
                 
             case REC_STREAM:
+                for (int i = 0; i < pkt_amount; i++) {
+                    buf = new byte[256];
+                    recv_packet = new DatagramPacket(buf, buf.length);
+                    try {
+                        socket.receive(recv_packet);
+                    } catch (Exception e) {
+                        System.out.println("No_pkts!");
+                    }  
+                    
+                received = new String(recv_packet.getData(), 0, recv_packet.getLength());
+                System.out.println("Recieved: " + received);
+                    
+                    if (received.startsWith("|")) {
+                        recvd_counter++;                   
+                        char[] array = received.toCharArray();
+                        int distance=0;
+                        //count distance between two '|' characters to find the length of the packet number
+                        //first one is always '|'
+                        for (int j = 1; j < array.length; j++) {
+                            if (array[j] != '|') {
+                                distance++;
+                            }
+                            else{
+                                //System.out.println(distance);
+                                //System.out.println(received.substring(1, distance+1));
+                                //System.out.println(received.substring(distance+2,received.length()));
+                                recv_seqList.add(received.substring(1, distance+1));
+                                recv_dataList.add(received.substring(distance+2,received.length()));
+                                break;
+                            }
+                            
+                        }
+                    }
+
+                
+                }
+
+                
                 break;
                 
             case IDLE:
